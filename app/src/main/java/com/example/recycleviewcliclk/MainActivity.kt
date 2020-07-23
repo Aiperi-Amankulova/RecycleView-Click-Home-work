245d846bda2ea3a4990e93a6bc86c2dc498ae3cf
Aiperi <aip.amankulova@gmail.com>
2020-07-23 22:48:03 +0600

    Add button

--- app/src/main/java/com/example/recycleviewcliclk/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewcliclk/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.recycleviewcliclk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
     }
-}
